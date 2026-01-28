94827a34e1b8699400453e9ef064d22fd74f819e
Adriel Vijuan <ayjeboi@gmail.com>
2026-01-27 16:26:34 -0800

    Fix missing useCallback import

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import { HistoricalEvent } from '../types';
